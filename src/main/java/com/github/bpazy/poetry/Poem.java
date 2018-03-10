610b6d9f09035045d0f4b8b9d317dadc3e45ca69
Ziyuan <onbpazy@163.com>
2018-03-10 14:42:35 +0800

    规则

--- src/main/java/com/github/bpazy/poetry/Poem.java
+++ src/main/java/com/github/bpazy/poetry/Poem.java
@@ -2,6 +2,7 @@ package com.github.bpazy.poetry;
 
 import com.google.common.collect.Lists;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class Poem {
     private List<Sentence> sentences = Lists.newArrayList();
 
     public Poem(String poem) {
+        poem = poem.replaceAll("\\s+", "");
         String[] sentences = poem.split("[,，。？?]");
         for (String sentence : sentences) {
             this.sentences.add(new Sentence(sentence));
